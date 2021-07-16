22e970478237a2373d7b121c2e481532e4110399
Mubashar Toseef <mrmobi62@gmail.com>
2021-07-17 00:23:31 +0500

    creating firebase auth app

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,62 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_2/screens/Electronics.dart';
-import 'package:flutter_application_2/screens/elist.dart';
-import 'screens/signup.dart';
+import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter_application_2/screens/Electronics.dart';
+// import 'package:flutter_application_2/screens/elist.dart';
+// import 'screens/signup.dart';
 
 // import 'nav.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 //" flutter run -d chrome --release" this command will work instead of flutter run
 class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Home());
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Text("You Got Error");
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(home: Login());
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Text("Loading...");
+      },
+    );
+    // return MaterialApp(home: Login());
   }
 }
 
-class Home extends StatelessWidget {
+class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Container(
-            child: Text(
-              "HOME PAGE",
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.white,
-          actions: [
-            Icon(
-              Icons.notifications,
-              color: Colors.black,
-              size: 30.0,
-            ),
+        body: SafeArea(
+            child: Container(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 30),
+            Text("hello dear"),
           ],
         ),
-        body: Container(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(height: 30),
-                ElevatedButton(
-                  // style: style,
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => SignUp()));
-                  },
-                  child: const Text('SIGNUP'),
-                ),
-                SizedBox(height: 30),
-                ElevatedButton(
-                  // style: style,
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => Electronics()));
-                  },
-                  child: const Text('ECOMMERCE'),
-                ),
-                SizedBox(height: 30),
-                ElevatedButton(
-                  // style: style,
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => ListViews()));
-                  },
-                  child: const Text('Listing'),
-                ),
-                Row(
-                  children: [],
-                )
-              ],
-            ),
-          ),
-        ));
+      ),
+    )));
   }
 }
+
 
 //     home: Scaffold(
 //   appBar: AppBar(
